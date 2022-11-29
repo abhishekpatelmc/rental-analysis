5780e12ae7ff58bc27f5a1b260701c18666a1759
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-28 23:19:08 -0500

    margin added

--- frontend/pages/RentPredict/rentPredict.jsx
+++ frontend/pages/RentPredict/rentPredict.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function rentPreict() {
+export default function rentPreict() {
   const [data, setData] = useState();
 
   const handelSubmit = async (event) => {
@@ -107,11 +107,9 @@ function rentPreict() {
         </div>
       </div>
 
-      <div className="text-center mt-1">
+      <div className="text-center mt-10">
         {data && <h1 className="text-2xl ">Rent Estimate: {data}</h1>}
       </div>
     </div>
   );
 }
-
-export default rentPreict;

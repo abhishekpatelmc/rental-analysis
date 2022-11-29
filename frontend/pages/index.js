1e4a70b108d8a6ad360c8d7209915d34b8cd74c1
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-28 23:07:33 -0500

    trying to fix

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ">
         <Link
           className=" p-5 bg-cyan-900 text-white rounded-xl"
-          href="\rentPredict\rentPredict"
+          href="/rentPredict/rentPredict"
         >
           React Predict
         </Link>

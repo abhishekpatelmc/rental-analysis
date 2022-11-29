7613c408bb640f4c512e8e0eaee0d69ca0a0e9a7
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-11-29 01:19:55 -0500

    backend folder added

---
+++ backend/fastApi.py
@@ -0,0 +1,53 @@
+# Building API 
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import pickle
+import json
+
+app = FastAPI()
+
+class RentItem(BaseModel):
+   region: str
+   sqfeet: float
+   baths: float
+   beds: float
+
+with open('./saved/rentAnalyser.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open("./saved/columns.json", "r") as f:
+    data_columns = json.load(f)['data_columns']
+
+def get_estimated_price(region, sqfeet, beds, baths):
+    try:
+        loc_index = data_columns.index(region.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(data_columns))
+    x[0] = sqfeet
+    x[1] = baths
+    x[2] = beds
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(model.predict([x])[0], 2)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Populate locations to the frontend
+@app.get("/getLocation")
+async def get_location():
+    locations = data_columns[3:]
+    return locations
+
+@app.post("/predict")
+async def rentModelEndpoint(item: RentItem):
+    region = item.region
+    sqfeet = item.sqfeet
+    baths = item.baths
+    beds = item.beds
+    prediction = get_estimated_price(region, sqfeet, beds, baths)
+    return {"price": prediction}
+
